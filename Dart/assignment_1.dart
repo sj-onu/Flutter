38fce3c30418e1f378ed5045500a410a5f7e19a3
Sabrina Jahan <onusabrina1998@gmail.com>
2023-04-17 22:40:06 +0600

    17/04/2023

--- Dart/assignment_1.dart
+++ Dart/assignment_1.dart
@@ -1,5 +1,5 @@
 main() {
-  List<String> phoneNumber = ["+88", "01768131685", "01768171985","+880", "01768111286", "01768131685"];
+  List<String> phoneNumber = ["+88", "01768131685", "01768171985","+88", "01768111286", "01768131685"];
   for (String number in phoneNumber) {
     if (number != "+88"){
       print('+88$number');
