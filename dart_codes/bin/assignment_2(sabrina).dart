55bc491eb15686f17fa3c701b35d4461f7ecc326
Sabrina Jahan <67582404+sj-onu@users.noreply.github.com>
2023-05-08 19:10:23 +0600

    final

--- dart_codes/bin/assignment_2(sabrina).dart
+++ dart_codes/bin/assignment_2(sabrina).dart
@@ -1,24 +1,26 @@
 main() {
 
   String studentName = "Sabrina Jahan";
-  int testScore = 75;
+  int testScore = 92;
 
   String grade = studentGrade(studentName, testScore);
 
-  print("$studentName's grade: $grade");
+  print("$grade");
 }
 
 String studentGrade(String studentName, int testScore) {
   if (testScore >= 90 && testScore <=100) {
-    return "${studentName}'s grade is A";
+    return "${studentName}'s grade is 'A'";
   } else if (testScore >= 80 && testScore <=89) {
-    return "${studentName}'s grade is B";
+    return "${studentName}'s grade is 'B'";
   } else if (testScore >= 70 && testScore <=79) {
-    return "${studentName}'s grade is C";
+    return "${studentName}'s grade is 'C'";
   } else if (testScore >= 60 && testScore <=69) {
-    return "${studentName}'s grade is D";
-  } else {
-    return "${studentName}'s grade is F";
+    return "${studentName}'s grade is 'D'";
+  } else if (testScore >= 0 && testScore <=59) {
+    return "${studentName}'s grade is 'F'";
+  } else{
+    return "Invalid";
   }
 }
 
