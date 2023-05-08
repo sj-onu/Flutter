a7a23074763f964e5a939d181d3e5b2682e5f692
Sabrina Jahan <67582404+sj-onu@users.noreply.github.com>
2023-05-08 19:12:12 +0600

    Update assignment_2(sabrina).dart

--- dart_codes/bin/assignment_2(sabrina).dart
+++ dart_codes/bin/assignment_2(sabrina).dart
@@ -10,15 +10,15 @@ main() {
 
 String studentGrade(String studentName, int testScore) {
   if (testScore >= 90 && testScore <=100) {
-    return "${studentName}'s grade is 'A'";
+    return "$studentName's grade is 'A'";
   } else if (testScore >= 80 && testScore <=89) {
-    return "${studentName}'s grade is 'B'";
+    return "$studentName's grade is 'B'";
   } else if (testScore >= 70 && testScore <=79) {
-    return "${studentName}'s grade is 'C'";
+    return "$studentName's grade is 'C'";
   } else if (testScore >= 60 && testScore <=69) {
-    return "${studentName}'s grade is 'D'";
+    return "$studentName's grade is 'D'";
   } else if (testScore >= 0 && testScore <=59) {
-    return "${studentName}'s grade is 'F'";
+    return "$studentName's grade is 'F'";
   } else{
     return "Invalid";
   }
